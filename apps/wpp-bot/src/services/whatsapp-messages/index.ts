a2afa7b4223d60e0d0c886beb4987472358a36f2
Diego Nascimento <dfnascimento@live.com>
2023-09-16 16:03:36 -0300

    feat: enable whatsapp auto close

--- apps/wpp-bot/src/services/whatsapp-messages/index.ts
+++ apps/wpp-bot/src/services/whatsapp-messages/index.ts
@@ -1,11 +1,8 @@
-
-
-
 const wppconnect = require( "@wppconnect-team/wppconnect");
 
 export const createServer = async () => {
 
-  const options = { session: "whatsbot", autoClose: false };
+  const options = { session: "whatsbot", autoClose: true };
   return wppconnect.create(options);
 };
 
