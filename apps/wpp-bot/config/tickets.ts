d4ad841f451d05dcf4d67149fd50efb053b207cf
Diego Nascimento <dfnascimento@live.com>
2023-10-28 10:43:47 -0300

    feat: add email env

--- apps/wpp-bot/config/tickets.ts
+++ apps/wpp-bot/config/tickets.ts
@@ -1,8 +1,5 @@
 export const ticket = {
-  gtic: {
-    email: process.env.GTIC_TICKET_EMAIL,
-  },
-  ascon: {
-    email: process.env.ASCON_TICKET_EMAIL,
-  },
+  gtic: { email: process.env.GTIC_TICKET_EMAIL },
+  ascon: { email: process.env.ASCON_TICKET_EMAIL },
+  administrativeConsultancy: { email: process.env.ASSESSORIA_ADM_TICKET_EMAIL },
 };
