5fac6a3af99ed070261042870538adb67b202682
Diego Nascimento <dfnascimento@live.com>
2023-10-28 09:59:32 -0300

    feat: add ascon chat flow

--- apps/wpp-bot/src/chat/entrypoint-state/index.ts
+++ apps/wpp-bot/src/chat/entrypoint-state/index.ts
@@ -1,13 +1,16 @@
-import { State } from '../interface'
-import { ENTRYPOINT_MENU_OPTIONS, EntrypointOptions } from '../../menu-options'
-import { GticMenuOptionsState } from '../gtic'
+import { State } from "../interface";
+import { ENTRYPOINT_MENU_OPTIONS, EntrypointOptions } from "../../menu-options";
+import { GticMenuOptionsState } from "../gtic";
+import { AsconMenuOptionsState } from "../ascon";
 
 export const InitialMenuState: State = {
   menu: ENTRYPOINT_MENU_OPTIONS,
   next: (choice) => {
-    switch(choice) {
+    switch (choice) {
       case EntrypointOptions.GTIC:
-        return GticMenuOptionsState
+        return GticMenuOptionsState;
+      case EntrypointOptions.ASCON:
+        return AsconMenuOptionsState;
     }
   },
 };
