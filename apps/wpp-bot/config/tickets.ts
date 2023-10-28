6ec13f28ce5247f80bb82f437128be3244f64af5
Diego Nascimento <dfnascimento@live.com>
2023-10-28 10:09:40 -0300

    feat: add ascon ticket creation

--- apps/wpp-bot/config/tickets.ts
+++ apps/wpp-bot/config/tickets.ts
@@ -2,4 +2,7 @@ export const ticket = {
   gtic: {
     email: process.env.GTIC_TICKET_EMAIL,
   },
+  ascon: {
+    email: process.env.ASCON_TICKET_EMAIL,
+  },
 };
--- apps/wpp-bot/src/services/tickets/index.ts
+++ apps/wpp-bot/src/services/tickets/index.ts
@@ -1,5 +1,7 @@
+import { AsconTicket } from "./ascon";
 import { GticTicket } from "./gtic";
 
 export const Ticket = {
   Gtic: GticTicket.getInstance(),
+  Ascon: AsconTicket.getInstance(),
 };
