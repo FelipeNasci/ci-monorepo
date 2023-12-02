c0737e7ba96d66e9638caddfbc6871b81ed0de72
Diego Felipe <36546304+FelipeNasci@users.noreply.github.com>
2023-12-02 06:48:58 -0300

    feat: add department menu options (#10)

--- apps/wpp-bot/src/controllers/chat-controller/university-town-hall/index.ts
+++ apps/wpp-bot/src/controllers/chat-controller/university-town-hall/index.ts
@@ -5,15 +5,28 @@ import {
   PU_DEPARTMENT_PHONE_REQUESTER,
   PU_MAINTENANCE_CATEGORY,
   PU_MAINTENANCE_DEPARTMENT,
+  PU_MAINTENANCE_DEPARTMENT_RTT,
+  PU_MAINTENANCE_DEPARTMENT_MME,
   PU_MAINTENANCE_MORE_DETAILS,
   EMaintenanceCategoryKind,
+  EMaintenanceDepartmentRtt,
+  EMaintenanceDepartmentMme,
 } from "../../../domain/menu-options/university-town-hall";
+import { LOCATION, ELocation } from "../../../domain/menu-options/sign-up";
 
 import { SUCCESS_ANSWER } from "../../../domain/menu-options/shared/success";
 import { extractValuesFromObject } from "../../../helpers";
 
 const maintenanceCategoryOptions = extractValuesFromObject<string>(
   EMaintenanceCategoryKind
+);
+
+const maintenanceDepartmentRttOptions = extractValuesFromObject<string>(
+  EMaintenanceDepartmentRtt
+);
+
+const maintenanceDepartmentMmeOptions = extractValuesFromObject<string>(
+  EMaintenanceDepartmentMme
 );
 
 export const puMaintenanceDepartmentKindState: State = {
@@ -28,13 +41,32 @@ export const puMaintenanceRegisterNumberState: State = {
 
 export const puMaintenanceDepartmentPhoneState: State = {
   menu: PU_DEPARTMENT_PHONE_REQUESTER,
-  next: () => puMaintenanceDepartmentState,
+  next: (_, ticket) => {
+    if (ticket?.unidade === LOCATION.options[ELocation.RTT])
+      return puMaintenanceDepartmentRttState;
+    return puMaintenanceDepartmentMmeState;
+  },
+};
+
+export const puMaintenanceDepartmentRttState: State = {
+  menu: PU_MAINTENANCE_DEPARTMENT_RTT,
+  next: (choice) =>
+    maintenanceDepartmentRttOptions.includes(choice) &&
+    puMaintenanceDepartmentState,
+};
+
+export const puMaintenanceDepartmentMmeState: State = {
+  menu: PU_MAINTENANCE_DEPARTMENT_MME,
+  next: (choice) =>
+    maintenanceDepartmentMmeOptions.includes(choice) &&
+    puMaintenanceDepartmentState,
 };
 
 export const puMaintenanceDepartmentState: State = {
   menu: PU_MAINTENANCE_DEPARTMENT,
   next: () => puMaintenanceCategoryState,
 };
+
 export const puMaintenanceCategoryState: State = {
   menu: PU_MAINTENANCE_CATEGORY,
   next: (choice) =>
