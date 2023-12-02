a3c10b090546e4b69bc7a0b87d1b6b970e7fa64f
Diego Nascimento <dfnascimento@live.com>
2023-12-02 08:01:06 -0300

    feat: add ticket interface

--- apps/wpp-bot/src/domain/index.ts
+++ apps/wpp-bot/src/domain/index.ts
@@ -12,6 +12,18 @@ export enum TicketDestination {
   UniversityTownHall = "university_town_hall",
 }
 
+export enum Location {
+  Rtt = "Rio Tinto",
+  Mme = "Mamanguape",
+}
+
+export enum UserType {
+  Professor = "Professor",
+  TAE = "TAE",
+  Student = "Student",
+  Other = "Other",
+}
+
 export type ChatUser = { phoneNumber: string; message: string };
 
 export type Request = Record<string, string>;
@@ -24,3 +36,29 @@ export type ActiveUsers = {
     previousStates?: State[];
   };
 };
+
+interface User {
+  name: string;
+  register: string;
+  userType: string;
+  email: string;
+}
+
+interface Destination {
+  department: `${TicketDestination}`;
+  phone: string;
+  location: `${Location}`;
+  block: string;
+  room: string;
+  specificPlace: string;
+}
+
+export interface Ticket {
+  user: User;
+  destination: Destination;
+
+  serviceType: string;
+  equipment: string;
+  equipmentId: string;
+  description: string;
+}
