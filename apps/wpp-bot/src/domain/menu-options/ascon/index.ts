f65ce8e966cefe0fb53760854579647bf1f1d7fb
Diego Felipe <36546304+FelipeNasci@users.noreply.github.com>
2024-01-03 13:13:45 -0300

    refactor: fix ASCOM tests

--- apps/wpp-bot/src/domain/menu-options/ascon/index.ts
+++ apps/wpp-bot/src/domain/menu-options/ascon/index.ts
@@ -2,5 +2,5 @@ import { MenuOptions } from "../interface";
 
 export const ASCON: MenuOptions = {
   className: "ASCON",
-  message: "Descreva qual a sua solicitação para a ASCON.",
+  message: "Descreva qual a sua solicitação para a ASCOM.",
 };
--- apps/wpp-bot/src/domain/menu-options/index.ts
+++ apps/wpp-bot/src/domain/menu-options/index.ts
@@ -3,16 +3,16 @@ import { MenuOptions } from "./interface";
 export enum EntrypointOptions {
   GTIC = "1",
   ADMINISTRATIVE_CONSULTANCY = "2",
-  ASCON = "3",
+  ASCOM = "3",
   SUBPREFEITRA = "4",
 }
 
 export const ENTRYPOINT_MENU_OPTIONS: MenuOptions = {
   message: `Olá, bem-vindo. \nCom qual setor você gostaria de falar?`,
   options: {
-    [EntrypointOptions.GTIC]: "GTIC",
+    [EntrypointOptions.GTIC]: "Gerência de Tecnologia da Informação e Comunicação",
     [EntrypointOptions.ADMINISTRATIVE_CONSULTANCY]: "Assessoria Administrativa",
-    [EntrypointOptions.ASCON]: "ASCON",
+    [EntrypointOptions.ASCOM]: "Assessoria de Comunicação",
     [EntrypointOptions.SUBPREFEITRA]: "Subprefeitura Universitária",
   },
 };
